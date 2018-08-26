51f178f96c5074e4c81d8b880eeb9a0ef9467094
Charles Martin Reed <charlesmartinreed@icloud.com>
2018-08-26 14:05:33 -0500

    Add Checkerboard and Transform effects.

--- Project27/ViewController.swift
+++ Project27/ViewController.swift
@@ -66,6 +66,83 @@ class ViewController: UIViewController {
         imageView.image = img
     }
     
+    func drawCheckerboard() {
+        //a different way of drawing rectangles is just to fill them directly with your target color
+        let renderer = UIGraphicsImageRenderer(size: CGSize(width: 512, height: 512))
+        
+        let img = renderer.image { (ctx) in
+            ctx.cgContext.setFillColor(UIColor.black.cgColor)
+            //building the rectangle - every other square is black. Sine we haven't set our renderer to opaque, the non-colored squares will be transparent
+            for row in 0..<8 {
+                for col in 0..<8 {
+                    if (row + col) % 2 == 0 {
+                        //skips path/draw work seen before and just fills the rectangle using whatever the current fill color is.
+                        ctx.cgContext.fill(CGRect(x: col * 64, y: row * 64, width: 64, height: 64))
+                    }
+                }
+            }
+        }
+        
+        imageView.image = img
+    }
+    
+    func drawRotatedquares() {
+        
+        let renderer = UIGraphicsImageRenderer(size: CGSize(width: 512, height: 512))
+        
+        let img = renderer.image { (ctx) in
+            //because the transformation matrix is top left corner of view, we need to mve the transform matrix to the half way point of our image in order to move the rotation point
+            ctx.cgContext.translateBy(x: 256, y: 256)
+            
+            let rotations = 16
+            let amount = Double.pi / Double(rotations)
+            
+            for _ in 0 ..< rotations {
+                //we can apply transforms to our context BEFORE drawing
+                //transforms are cumulative, that's what makes this code work to create this beautiful square
+                ctx.cgContext.rotate(by: CGFloat(amount))
+                ctx.cgContext.addRect(CGRect(x: -128, y: -128, width: 256, height: 256))
+            }
+            
+            ctx.cgContext.setStrokeColor(UIColor.black.cgColor)
+            ctx.cgContext.strokePath()
+        }
+        
+        imageView.image = img
+    }
+    
+    func drawLines() {
+        //draws boxes within boxes, always getting smaller
+        //adding a line to more or less that same point inside a loop, but the loop rotates the context transform so the line end point moves as well
+        //line length slowly decreases, causing space between boxes to shrink
+        let renderer = UIGraphicsImageRenderer(size: CGSize(width: 512, height: 512))
+        
+        let img = renderer.image { (ctx) in
+            ctx.cgContext.translateBy(x: 256, y: 256)
+            
+            var first = true
+            var length: CGFloat = 256
+            
+            for _ in 0 ..< 256 {
+                ctx.cgContext.rotate(by: CGFloat.pi / 2)
+                
+                if first {
+                    ctx.cgContext.move(to: CGPoint(x: length, y: 50))
+                    first = false
+                } else {
+                    ctx.cgContext.addLine(to: CGPoint(x: length, y: 50))
+                }
+                
+                length *= 0.99
+            }
+            
+            ctx.cgContext.setStrokeColor(UIColor.black.cgColor)
+            ctx.cgContext.strokePath()
+        }
+        
+        imageView.image = img
+    }
+    
     @IBAction func redrawTapped(_ sender: Any) {
         //+=1 to the currentDrawType, set to 0 when it reaches a certain point
         //use switch/case on the property to determine which method to call
@@ -82,6 +159,15 @@ class ViewController: UIViewController {
             
         case 1:
             drawCircle()
+            
+        case 2:
+            drawCheckerboard()
+            
+        case 3:
+            drawRotatedquares()
+            
+        case 4:
+            drawLines()
             
         default:
             break
